4bf1d3cc8642a2925fc8c95cfb6b1491b216f6d9
Norio Nomura <norio.nomura@gmail.com>
2018-04-22 13:38:06 +0900

    Add `upload(_:as:)` that uploads file to https://file.io/

---
+++ Sources/SwiftCompilerDiscordappBot/upload().swift
@@ -0,0 +1,75 @@
+//
+//  upload().swift
+//  SwiftCompilerDiscordappBot
+//
+//  Created by Norio Nomura on 4/22/18.
+//
+
+import Dispatch
+import Foundation
+
+let uploaderURL = URL(string: "https://file.io/")!
+
+func upload(_ text: String?, as filename: String) -> String? {
+    guard let text = text else { return nil }
+
+    var request = URLRequest(url: uploaderURL)
+    request.httpMethod = "POST"
+
+    let boundary = UUID().uuidString
+    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+    var data = Data()
+    data.append("--\(boundary)\r\n")
+    data.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n")
+    data.append("Content-Type: text/plain\r\n\r\n")
+    data.append(text)
+    data.append("\r\n--\(boundary)--\r\n")
+
+    var uploadedLink: String? = nil
+    let semaphore = DispatchSemaphore(value: 0)
+    let task = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
+        defer { semaphore.signal() }
+        if let error = error {
+            App.log("failed to upload stdout `stdout.text` with error: \(error)")
+            return
+        }
+        guard let httpURLResponse = response as? HTTPURLResponse else {
+            App.log("unknown response: \(String(describing: response))")
+            return
+        }
+        guard (200...299).contains(httpURLResponse.statusCode) else {
+            App.log("server error status: \(httpURLResponse.statusCode)")
+            return
+        }
+        guard let data = data else {
+            App.log("failed to unwrap data returned from file.io")
+            return
+        }
+        do {
+            struct Payload: Decodable {
+                var success: Bool
+                var key: String
+                var link: String
+                var expiry: String
+            }
+            let payload = try JSONDecoder().decode(Payload.self, from: data)
+            if payload.success {
+                uploadedLink = payload.link
+            }
+        } catch {
+            App.log("failed to decode payload with error: \(error)")
+        }
+    }
+    task.resume()
+    semaphore.wait()
+
+    return uploadedLink
+}
+
+extension Data {
+    mutating func append(_ string: String) {
+        guard let data = string.data(using: .utf8) else { return }
+        append(data)
+    }
+}
