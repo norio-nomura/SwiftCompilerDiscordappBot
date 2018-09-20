13e38c3139c147d79caee56c64967520b3081b34
Norio Nomura <norio.nomura@gmail.com>
2018-09-21 08:46:14 +0900

    Re-add imports to Libraries.swift

--- Libraries/Sources/Libraries/Libraries.swift
+++ Libraries/Sources/Libraries/Libraries.swift
@@ -1,3 +1,22 @@
+#if swift(>=4.2)
+    #if canImport(RxSwift)
+        import RxSwift
+    #endif
+    #if canImport(SwiftBacktrace)
+        import SwiftBacktrace
+    #endif
+    #if canImport(SwiftyMath)
+        import SwiftyMath
+    #endif
+    #if canImport(Vapor)
+        import Vapor
+    #endif
+#else
+    import RxSwift
+    import SwiftBacktrace
+    import Vapor
+#endif
+
 struct Libraries {
     var text = "Hello, World!"
 }
