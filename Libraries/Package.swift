afec6c1fab25fedc8343b31fb5b6a4164bbf9f5d
Norio Nomura <norio.nomura@gmail.com>
2020-02-28 10:21:24 +0900

    Add Swift Argument Parser to Libraries

--- Libraries/Package.swift
+++ Libraries/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,14 +17,35 @@ var package = Package(
         .package(url: "https://github.com/norio-nomura/SwiftBacktrace", .branch("master"))
     ],
     targets: [
-        .target(
-            name: "Libraries",
-            dependencies: ["NIO", "NIOTLS", "NIOHTTP1", "NIOConcurrencyHelpers", "NIOFoundationCompat", "NIOWebSocket", "SwiftBacktrace", "StandardLibraryPreview"]),
-        .target(
-            name: "Run",
-            dependencies: ["Libraries"]),
-        .testTarget(
-            name: "LibrariesTests",
-            dependencies: ["Libraries"])
+         .target(
+             name: "Run",
+             dependencies: ["Libraries"])
     ]
 )
+
+let librariesTarget = Target.target(
+    name: "Libraries",
+    dependencies: [
+        .product(name: "NIO", package: "swift-nio"),
+        .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+        .product(name: "NIOFoundationCompat", package: "swift-nio"),
+        .product(name: "NIOHTTP1", package: "swift-nio"),
+        .product(name: "NIOTLS", package: "swift-nio"),
+        .product(name: "NIOWebSocket", package: "swift-nio"),
+        .product(name: "StandardLibraryPreview", package: "swift-standard-library-preview"),
+        .product(name: "SwiftBacktrace", package: "SwiftBacktrace"),
+    ]
+)
+
+package.targets.append(librariesTarget)
+
+// SE-0226 Package Manager Target Based Dependency Resolution is required to avoid error:
+// ```
+// error: multiple targets named 'TestHelpers' in: swift-argument-parser, swift-se0270-range-set
+// ```
+#if compiler(>=5.2)
+
+package.dependencies.append(.package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"))
+librariesTarget.dependencies.append(.product(name: "ArgumentParser", package: "swift-argument-parser"))
+
+#endif
